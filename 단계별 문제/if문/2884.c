6a5c490ce3b75d34d14238022576e99bac524984
RAY <80562784+Aram-Yun@users.noreply.github.com>
2021-12-24 18:25:20 +0900

    push

---
+++ 단계별 문제/if문/2884.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+  int H = 0, M = 0;
+  scanf("%d %d", &H, &M);
+
+  if(M >= 45) {
+    printf("%d %d", H, M - 45);
+  }
+  else {
+    if(H == 0) {
+      printf("%d %d", 23, M + 15);
+    }
+    else {
+      printf("%d %d", H - 1, M + 15);
+    }
+  }
+  return 0;
+}

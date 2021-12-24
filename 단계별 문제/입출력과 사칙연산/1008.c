4ce6049014cc49a3f9adb5486b5382232efbfbe8
RAY <80562784+Aram-Yun@users.noreply.github.com>
2021-12-24 18:11:21 +0900

    push

---
+++ 단계별 문제/입출력과 사칙연산/1008.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+  double A = 0, B = 0;
+  scanf("%d %d", &A, &B);
+  printf("%.9lf", A / B);
+
+  return 0;
+}
---
+++ 단계별 문제/입출력과 사칙연산/10869.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+  int A = 0, B = 0;
+  scanf ("%d %d", &A, &B);
+  printf ("%d\n%d\n%d\n%d\n%d", A+B, A-B, A*B, A/B, A%B);
+
+  return 0;
+}
